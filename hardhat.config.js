a6d339655942b6ca1a72c7b8f9bb39b3c1108ed3
BaldyAsh <antongrigorjev2010@gmail.com>
2022-03-24 18:05:54 +0400

    Tested deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,12 +17,22 @@ module.exports = {
     },
     goerli: {
       url: `https://goerli.infura.io/v3/31c3397beb4146e4acc9f4a072da5d23`,
-      accounts: ["0x7ff8c90cb78ec105f8987f450cf0df4a66dea8497df750f1fec1ecb1b789336a"],
+      accounts: [
+        `0x7ff8c90cb78ec105f8987f450cf0df4a66dea8497df750f1fec1ecb1b789336a`,
+        `0xeee42d472b027d6efbf8b54e6d824b457c623e5b0ca3f3ac9210a9ec4b865e31`,
+        `0x490f59212fee8a04a18ab4f93d85d7fa8246164b16fa861ee8578f1099ba2f2f`,
+        `0xa4169cbd5cbd698d5cbb3e6460500846ec647ac6f5e6290bb57b3f1cba1dd4ff`,
+      ],
       timeout: 100000
     },
     kovan: {
       url: `https://kovan.infura.io/v3/31c3397beb4146e4acc9f4a072da5d23`,
-      accounts: [`0x7ff8c90cb78ec105f8987f450cf0df4a66dea8497df750f1fec1ecb1b789336a`],
+      accounts: [
+        `0x7ff8c90cb78ec105f8987f450cf0df4a66dea8497df750f1fec1ecb1b789336a`,
+        `0xeee42d472b027d6efbf8b54e6d824b457c623e5b0ca3f3ac9210a9ec4b865e31`,
+        `0x490f59212fee8a04a18ab4f93d85d7fa8246164b16fa861ee8578f1099ba2f2f`,
+        `0xa4169cbd5cbd698d5cbb3e6460500846ec647ac6f5e6290bb57b3f1cba1dd4ff`,
+      ],
       timeout: 100000
     },
     ropsten: {
